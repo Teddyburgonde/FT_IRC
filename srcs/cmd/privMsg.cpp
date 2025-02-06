7471c19cab21bf25d01ad4052e4abc5c4e933cf2
Galaad Mersch <gmersch@1G2.42angouleme.fr>
2025-02-06 15:58:00 +0100

    fix PRIVMSG and small things

--- srcs/cmd/privMsg.cpp
+++ srcs/cmd/privMsg.cpp
@@ -9,13 +9,13 @@ void Server::handlePrivMsg(int fd, Message &msg, std::vector<Channel> &_channel)
 
 	index = 0;
 	// Trouver le destinataire
-	size_t spacePos = msg.getArgument().find(' ', 8);
-	if (spacePos == std::string::npos)
-	{
-		std::string response = ERR_NORECIPIENT(std::string ("Server"), "");
-		send(fd, response.c_str(), response.size(), 0);
-		return;
-	}
+	// size_t spacePos = msg.getArgument().find(' ', 8);
+	// if (spacePos == std::string::npos)
+	// {
+	// 	std::string response = ERR_NORECIPIENT(std::string ("Server"), "");
+	// 	send(fd, response.c_str(), response.size(), 0);
+	// 	return;
+	// }
 	if (msg.getArgument().empty())
 	{
     	std::cout << "Aucun argument dans le message" << std::endl;
@@ -29,6 +29,13 @@ void Server::handlePrivMsg(int fd, Message &msg, std::vector<Channel> &_channel)
 		return;
 	}
 	std::string message = get_next_argument(msg.getArgument().c_str(), index);
+	if (message.empty())
+	{
+		std::string response = ERR_NOTEXTTOSEND(std::string("Server"));
+		send(fd, response.c_str(), response.size(), 0);
+		return;
+	}
+	message += "\n";
 	if (recipient[0] == '#')
 	{
 		std::vector<Channel>::iterator it_channel_to_send = find_channel_with_name(recipient, _channel);
@@ -38,29 +45,24 @@ void Server::handlePrivMsg(int fd, Message &msg, std::vector<Channel> &_channel)
 			send(fd, response.c_str(), response.size(), 0);
 			return;
 		}
-		(*it_channel_to_send).sendMessageToChannel(fd, message);
-		return;
-	}
-	if (message.empty())
-	{
-		std::string response = ERR_NOTEXTTOSEND(std::string("Server"));
-		send(fd, response.c_str(), response.size(), 0);
+		//si le userSender est pas dans le channel en question
+		if (!is_user_in_chan(fd, it_channel_to_send->getUserInChannel()))
+		{
+			send_error(ERR_NOTONCHANNEL(find_nickname_with_fd(fd, _clients), it_channel_to_send->getName()), fd);
+			return;
+		}
+		it_channel_to_send->sendMessageToChannel(fd, message);
 		return;
 	}
 	// Envoyer le message au destinataire
-	bool recipientFound = false;
 	for (size_t i = 0; i < _clients.size(); i++)
 	{
 		if (_clients[i].getNickname() == recipient)
 		{
 			send(_clients[i].getFd(), message.c_str(), message.size(), 0);
-			recipientFound = true;
-			break;
+			return;
 		}
 	}
-	if (!recipientFound)
-	{
-		std::string response = ERR_NOSUCHNICK(std::string("Server"), recipient);
-		send(fd, response.c_str(), response.size(), 0);
-	}
+	std::string response = ERR_NOSUCHNICK(std::string("Server"), recipient);
+	send(fd, response.c_str(), response.size(), 0);
 }
